f2ba64f7d535e04ab60483a810444970ad133ff1
Alberto Nicoletti <illbexyz@users.noreply.github.com>
2024-09-19 10:46:30 +0200

    feat: add importMeta = false

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.parser.javascript.importMeta = false;
+
+    return config;
+  },
 };
 
 export default nextConfig;
